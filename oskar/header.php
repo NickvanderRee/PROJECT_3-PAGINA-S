eb22e06cbabe92ced4c0f99748ca36e89973f87f
Oskar <112868301+9020078@users.noreply.github.com>
2023-04-03 15:08:11 +0200

    adyugwyau

---
+++ oskar/header.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <link rel="stylesheet" href="header.css">
+</head>
+<header>
+  <nav>
+    <ul>
+      <li><a href="homepage.php">Home</a></li>
+      <li><a href="Erim.php">Winkelen</a></li>
+      <li><a href="Milan.html">over ons</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><i class="fa fa-shopping-cart" style="font-size:48px;color:white"></i></li>
+    </ul>
+  </nav>
+</header>
+</html>
---
+++ oskar/over-ons.php
@@ -0,0 +1,71 @@
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​We believe in collaboration and innovation.">
+    <meta name="description" content="">
+    <title>Page 2</title>
+    <link rel="stylesheet" href="//capp.nicepage.com/b9586bafd8ee83dfafbe482833e68776ccac558c/nicepage.css" media="screen">
+    <link rel="stylesheet" href="/nicepage-site.css" media="screen">
+    <link rel="stylesheet" href="styleoverons.css" media="screen">
+    <script class="u-script" type="text/javascript" src="//capp.nicepage.com/assets/jquery-3.5.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="//capp.nicepage.com/b9586bafd8ee83dfafbe482833e68776ccac558c/nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.6.4, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+    <meta name="theme-color" content="#deca97">
+    <meta property="og:title" content="Page 2">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode" data-lang="en">
+    <section class="u-clearfix u-container-align-center u-section-1" id="carousel_13af">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-border-2 u-border-grey-15 u-border-no-bottom u-border-no-left u-border-no-right u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <h4 class="u-text u-text-3"> Erim</h4>
+                <p class="u-text u-text-4"><br>Being in the people business can mean difficult decisions and tough conversations. We believe in going into every meeting with a desire and ability to be genuine and compassionate. We are all people, and people make mistakes. We’re here to help you fix those, without risk or judgment, and move forward.</p>
+              </div>
+            </div>
+            <div class="u-border-2 u-border-grey-15 u-border-no-bottom u-border-no-left u-border-no-right u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-2">
+                <h4 class="u-text u-text-5"> Nick</h4>
+                <p class="u-text u-text-6"><br>Tempus egestas sed sed risus pretium quam vulputate dignissim. Ipsum consequat nisl vel pretium lectus quam id leo in. In pellentesque massa placerat duis ultricies lacus. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Etiam sit amet nisl purus in mollis.</p>
+              </div>
+            </div>
+            <div class="u-border-2 u-border-grey-15 u-border-no-bottom u-border-no-left u-border-no-right u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-3">
+                <h4 class="u-text u-text-7"> Oskar</h4>
+                <p class="u-text u-text-8"><br>Tempus egestas sed sed risus pretium quam vulputate dignissim. Ipsum consequat nisl vel pretium lectus quam id leo in. In pellentesque massa placerat duis ultricies lacus. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Etiam sit amet nisl purus in mollis.</p>
+              </div>
+            </div>
+            <div class="u-border-2 u-border-grey-15 u-border-no-bottom u-border-no-left u-border-no-right u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-4">
+                <h4 class="u-text u-text-9"> Lucas</h4>
+                <p class="u-text u-text-10"><br><br>Tempus egestas sed sed risus pretium quam vulputate dignissim. Ipsum consequat nisl vel pretium lectus quam id leo in. In pellentesque massa placerat duis ultricies lacus. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Etiam sit amet nisl purus in mollis.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <?php
+    include 'footer.php';
+    ?>
+    
+    
+  
+</body></html>
